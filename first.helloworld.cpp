94cbcb84333b9f34a0e142a2976a7ca9322f8d4d
ftpulkit <137203584+ftpulkit@users.noreply.github.com>
2023-12-25 12:09:11 +0530

    Update first.helloworld.cpp

--- first.helloworld.cpp
+++ first.helloworld.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-using namespace std;
-int main(){
-  cout<<hello world;
-    return 0;
+#include <iostream>//header file
+using namespace std; // includes cin,cout feature, in alternative :- std::cout<<" Hello world"  may also be used.
+int main(){ //this the starting and ending of the program i.e. the main function
+  cout<<hello world; // cout prints the output
+    return 0; //returning 0
 }
